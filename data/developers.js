b7a7348379bf0ecf2b179f4bf5fb9024d8f12f67
kiibra <kibra99@gmail.com>
2024-04-09 19:24:08 -0400

    complete edits to developers data & display data in browser

---
+++ data/developers.js
@@ -0,0 +1,10 @@
+const developers = [
+  { name: 'Seun', cohort: '3/18', state: 'VA'},
+  { name: 'Doribel', cohort: '3/18', state: 'MA'},
+  { name: 'Marie', cohort: '3/18', state: 'ATX'},
+  { name: 'Kibra', cohort: '3/18', state: 'IN'}
+];
+
+export{
+  developers
+}
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // import modules
 
 import express from 'express'
-
+import { developers } from './data/developers.js'
 // create Express app
 
 const app = express()
@@ -18,7 +18,11 @@ app.get('/home', function (req, res) {
   res.render('home')
 })
 
-
+app.get('/developers', function (req, res) {
+  res.render('developers/index', {
+    developers: developers
+  })
+})
 // tell the app to listen on port 3000
 
 app.listen(3000, function() {
