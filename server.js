3a355d95a1381a1e679ddd701f798aa6b0c02001
kiibra <kibra99@gmail.com>
2024-04-09 18:44:23 -0400

    complete creating data folder & file w/ data

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ app.set('view engine', 'ejs')
 // mount Middleware (app.use)
 
 
-
 // mount routes
-
+app.get('/home', function (req, res) {
+  res.render('home')
+})
 
 
 // tell the app to listen on port 3000
